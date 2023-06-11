9019c87d009243d97b010a49aa37c8339ece72c0
Yekpahi Prince <princeyekpahi@gmail.com>
2023-06-12 01:36:14 +0200

    Design

--- app/src/main/java/fr/prince/jetcompsesocial/MainActivity.kt
+++ app/src/main/java/fr/prince/jetcompsesocial/MainActivity.kt
@@ -3,13 +3,19 @@ package fr.prince.jetcompsesocial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import fr.prince.jetcompsesocial.ui.theme.JetCompseSocialTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,8 +36,31 @@ class MainActivity : ComponentActivity() {
 data class  Message(val author : String, val body : String)
 @Composable
 fun MessageCard(msg: Message) {
-   Text(text = msg.author)
-    Text(text = msg.body)
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(width = 8.dp))
+        Column() {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = msg.body,
+                style = MaterialTheme.typography.body2
+
+            )
+        }
+    }
+  
 }
 
 @Preview(showBackground = true)
